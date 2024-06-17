10a9f0d16540fd1fc696410962c1a0cd39db7ff3
Ho Gael <dt.gael.ho@gmail.com>
2024-06-17 16:17:48 +0200

    exoTODO javascript exercice guidé

---
+++ exoJavaScript/exoTODO_JS/script.js
@@ -0,0 +1,26 @@
+const addBtn = document.querySelector('#btn');
+addBtn.addEventListener('click',addTask);
+const taskCard = document.querySelector('.todoCard');
+const tasksContainer = document.querySelector("#todoCards");
+
+const delBtn = document.querySelector('.delBtn');
+delBtn.addEventListener('click', function(){
+    deleteTask(taskCard);
+});
+
+function addTask(){
+    const newTask = taskCard.cloneNode(true)//clone the task card
+    const newDelBtn = newTask.querySelector('.delBtn')
+    const newTextArea = newTask.querySelector('.task')
+
+    newTextArea.value = "New Task"
+    newDelBtn.addEventListener('click', function(){
+        deleteTask(newTask);
+    });
+
+    tasksContainer.appendChild(newTask)
+}
+
+function deleteTask(task){
+    task.remove();
+}
