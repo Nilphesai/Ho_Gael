7e77e912dfe73af9100847f2d7b539962db0199d
Ho Gael <dt.gael.ho@gmail.com>
2024-06-17 16:59:57 +0200

    exoTODO javascript : faire le count (en cours)

--- exoJavaScript/exoTODO_JS/script.js
+++ exoJavaScript/exoTODO_JS/script.js
@@ -8,17 +8,26 @@ delBtn.addEventListener('click', function(){
     deleteTask(taskCard);
 });
 
+function countTask(){
+    const countCard = document.querySelector('#count'); //l'element count
+    const listTask = tasksContainer.querySelectorAll('.todoCard');//tous les todoCard dans tasksContainer
+    for (let i = 0;i <= listTask.length();i++){//pour tous les element de listTask
+        countCard.innerText = "task = "+i; //écrire dans countCard
+    }
+}
+
 function addTask(){
     const newTask = taskCard.cloneNode(true)//clone the task card
     const newDelBtn = newTask.querySelector('.delBtn')
     const newTextArea = newTask.querySelector('.task')
-
+    
     newTextArea.value = "New Task"
     newDelBtn.addEventListener('click', function(){
         deleteTask(newTask);
     });
 
     tasksContainer.appendChild(newTask)
+    countTask();
 }
 
 function deleteTask(task){
