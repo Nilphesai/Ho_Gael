b5cb7a428b6de6216148da2c010c712591bac3fb
Ho <dt.gael.ho@gmail.com>
2024-06-19 15:47:32 +0200

    exo 4 : JS fait, modification du CSS en fonction des icones

--- exoJavaScript/exo4/script.js
+++ exoJavaScript/exo4/script.js
@@ -1,14 +1,30 @@
 const carres = document.querySelectorAll('.carre');
-let rouge = false;
+
 
 carres.forEach((carre) => {
     carre.addEventListener("click",() => {
-        if (carre.querySelector('.titre').style.visibility == "hidden"){
-
-            alert("ça marche");
-            //carre.classList.toggle('active');
+        if (getComputedStyle(carre.querySelector('.titre')).visibility !== 'visible'){
+            addStyle(carre);
         }
-        
-      
+        else{
+            deleteStyle(carre);
+        }
      });
   });
+
+  function addStyle(carre){
+    const styleCarre = getComputedStyle(carre);
+    document.querySelector('#wrapper').style.backgroundColor = styleCarre.backgroundColor;
+    carre.style.borderRadius = "20%";
+    const styleTexte = carre.querySelector('.titre');
+    styleTexte.style.visibility = "visible";
+    carre.style.boxShadow = "6px 0 6px 0px black inset";
+  }
+
+  function deleteStyle(carre){
+    document.querySelector('#wrapper').style.backgroundColor = "gray";
+    carre.style.borderRadius = "0";
+    const styleTexte = carre.querySelector('.titre');
+    styleTexte.style.visibility = "hidden";
+    carre.style.boxShadow = "0 0 0 0";
+  }
