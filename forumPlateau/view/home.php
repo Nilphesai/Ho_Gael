cfd717d06c3def68aa0573b9e07453e21dcafc8d
Ho <dt.gael.ho@gmail.com>
2024-07-11 11:59:53 +0200

    appli forum : CSS rajout flexbox

--- forumPlateau/view/home.php
+++ forumPlateau/view/home.php
@@ -1,4 +1,4 @@
-<div class="jumbotron">
+<div class="jumbotron" id='home'>
 <h1 class="display-3">BIENVENUE SUR LE FORUM</h1>
 
 <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
--- forumPlateau/view/layout.php
+++ forumPlateau/view/layout.php
@@ -10,7 +10,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
         
         <link rel="stylesheet" href="<?= PUBLIC_DIR ?>css/bootstrap.css">
-        
+        <link rel="stylesheet" href="<?= PUBLIC_DIR ?>css/style.css">
         <title>FORUM</title>
     </head>
     <body>
